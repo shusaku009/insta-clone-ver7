2ce97f0b76d3ec1e1861539b39f72d44429a1fe9
shusaku009 <shusaku009@gmail.com>
2022-10-26 23:01:23 +0900

    add ログイン機能実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
     
+    email { Faker::Internet.unique.email }
+    password { '12345678' }
+    password_confirmation { '12345678' }
+    username { Faker::Name.unique.name }
   end
 end
