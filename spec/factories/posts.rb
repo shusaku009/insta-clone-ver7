385519aca4ad6993a580ed5a88f79183c4930f05
shusaku009 <shusaku009@gmail.com>
2022-11-10 10:26:57 +0900

    add 投稿のCRUDのテストを書いた

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -18,6 +18,10 @@
 #
 FactoryBot.define do
   factory :post do
-    
+    body { Faker::Lorem.sentence }
+    user
+    after(:build) do |post|
+      post.images.attach(io: File.open('spec/fixtures/dummy.jpg'), filename: 'dummy.jpg', content_type: 'image/jpeg')
+    end
   end
 end
