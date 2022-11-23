d798e78c5eee3169c9e5623151ce5be656d79763
shusaku009 <shusaku009@gmail.com>
2022-11-23 11:04:57 +0900

    add フォロー, アンフォローのメソッドを作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+           foreign_key: 'followed_id',
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
@@ -42,5 +50,17 @@ class User < ApplicationRecord
 
   def like?(post)
     like_posts.include?(post)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.destroy(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
