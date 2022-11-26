b46967f260038c2248bb95e56a0cb7d379b3d6ab
shusaku009 <shusaku009@gmail.com>
2022-11-26 20:23:03 +0900

    add ユーザー一覧ページ

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -23,5 +23,5 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
 
-  validates :followed_id, niqueness: { scope: :followed_id }
+  validates :follower_id, uniqueness: { scope: :followed_id }
 end
