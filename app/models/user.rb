20620762927910cde4aca2943bd2e4329fcafa78
shusaku009 <shusaku009@gmail.com>
2022-11-26 09:22:14 +0900

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,13 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
   has_many :active_relationships, class_name: 'Relationship',
-           foreign_key: 'follower_id',
-           dependent: :destroy
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy,
+                                  inverse_of: :follower
   has_many :passive_relationships, class_name: 'Relationship',
-           foreign_key: 'followed_id',
-           dependent: :destroy
+                                  foreign_key: 'followed_id',
+                                  dependent: :destroy,
+                                  inverse_of: :followed
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
