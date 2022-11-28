c6f038df96b9b295efa3ecbb9ef9e4928ede5e89
shusaku009 <shusaku009@gmail.com>
2022-11-28 16:17:13 +0900

    modify ログイン時はフォローしている人の投稿と自分の投稿だけ表示させるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,9 @@ class User < ApplicationRecord
                                   dependent: :destroy,
                                   inverse_of: :follower
   has_many :passive_relationships, class_name: 'Relationship',
-                                  foreign_key: 'followed_id',
-                                  dependent: :destroy,
-                                  inverse_of: :followed
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy,
+                                   inverse_of: :followed
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
@@ -66,5 +66,9 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def feed
+    Post.where(user_id: following_ids << id)
   end
 end
