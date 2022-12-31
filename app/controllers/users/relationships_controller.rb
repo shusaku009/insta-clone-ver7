a1324e7025c942a40c248ae64ec1097267a43d03
shusaku009 <shusaku009@gmail.com>
2022-12-31 11:04:30 +0900

    add フォロー時の通知

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -10,4 +10,11 @@ class Users::RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
   end
+
+  private
+
+  def create_notifications_about_follow(user)
+    notification = Notification.create!(title: "#{current_user.user_name}さんにフォローされました", url: user_url(current_user))
+    notification.notify(user)
+  end
 end
