db3544d77faff1a44b942c4a8f37ed3a22791dc4
shusaku009 <shusaku009@gmail.com>
2023-01-26 17:03:26 +0900

    fix rubocop

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -6,11 +6,10 @@ class Posts::CommentsController < ApplicationController
     # rubocop:disable Style/GuardClause
     if @comment.save
       create_notifications_about_comment_to_own_post(@comment)
-      UserMailer.with(
-        user_from: current_user,
-        user_to: @comment.post.user,
-        comment: @comment
-      ).comment_post.deliver_later if @comment.post.user.accepted_notification?(:on_commented)
+      if @comment.post.user.accepted_notification?(:on_commented)
+        UserMailer.with(user_from: current_user, user_to: @comment.post.user,
+                        comment: @comment).comment_post.deliver_later
+      end
     end
     # rubocop:enable Style/GuardClause
   end
